c9836b3542702f08c46db10969ff789843b6e0c7
John Yandrapati <john.yandrapati@outlook.com>
2021-01-07 20:49:39 +0000

    Inspec

---
+++ tests/controls/test.rb
@@ -0,0 +1,18 @@
+control "world-1.0" do                                # A unique ID for this control
+    impact 1.0                                          # Just how critical is
+    title "Hello World"                                 # Readable by a human
+    desc "Text should include the words 'hello world'." # Optional description
+
+    describe directory('modules') do
+        it 'This directory should exist and be a directory.' do
+            expect(subject).to(exist)
+            expect(subject).to(be_directory)
+          end
+        end
+
+    describe file('modules/generic/hello.txt') do                       # The actual test
+     its('content') { 
+            should match 'Hello World' 
+        }      # You could just do the "describe file" block if you want. The  
+    end                                                 # rest is just metadata, but it's a good habit to get into.
+end
